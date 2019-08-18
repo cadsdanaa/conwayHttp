fdbfdb5973d46d22c3dc1a55eab5fc0856a7ad63
Alex <cadsdanaa@gmail.com>
2019-08-18 17:37:51 -0500

    Added Entity accessors.

--- conway/conway.go
+++ conway/conway.go
@@ -4,3 +4,23 @@ package conway
 type Entity struct {
 	Up, Down, Left, Right *Entity
 }
+
+//UpEntity returns the reference to the neighbor above the calling entity
+func (entity *Entity) UpEntity() *Entity {
+	return entity.Up
+}
+
+//DownEntity returns the reference to the neighbor below the calling entity
+func (entity *Entity) DownEntity() *Entity {
+	return entity.Down
+}
+
+//LeftEntity returns the reference to the neighbor left of the calling entity
+func (entity *Entity) LeftEntity() *Entity {
+	return entity.Left
+}
+
+//RightEntity returns the reference to the neighbor right of the calling entity
+func (entity *Entity) RightEntity() *Entity {
+	return entity.Right
+}
--- conway/conway_test.go
+++ conway/conway_test.go
@@ -6,9 +6,67 @@ import (
 
 func TestEntityShouldHaveReferenceToNeighbors(t *testing.T) {
 	var entity Entity
-	if entity.Up == nil && entity.Left == nil && entity.Right == nil && entity.Down == nil {
-		t.Log("Entity struct created")
-	} else {
+	if entity.Up != nil || entity.Left != nil || entity.Right != nil || entity.Down != nil {
 		t.Fail()
+	}
+}
+
+func TestShouldGetUpEntity(t *testing.T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.Up = &someOtherEntity
+
+	actualEntity := someEntity.UpEntity()
+
+	if actualEntity == nil {
+		t.Error("Up entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong up entity returned")
+	}
+}
+
+func TestShouldGetDownEntity(t *testing.T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.Down = &someOtherEntity
+
+	actualEntity := someEntity.DownEntity()
+
+	if actualEntity == nil {
+		t.Error("Up entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong up entity returned")
+	}
+}
+
+func TestShouldGetLeftEntity(t *testing.T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.Left = &someOtherEntity
+
+	actualEntity := someEntity.LeftEntity()
+
+	if actualEntity == nil {
+		t.Error("Up entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong up entity returned")
+	}
+}
+
+func TestShouldGetRightEntity(t *testing.T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.Right = &someOtherEntity
+
+	actualEntity := someEntity.RightEntity()
+
+	if actualEntity == nil {
+		t.Error("Up entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong up entity returned")
 	}
 }
