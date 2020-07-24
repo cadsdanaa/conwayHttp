de742d9c886f6e193bd52b934264d5db169b96fd
Alex Dana <cadsdanaa@gmail.com>
2020-07-24 16:03:38 -0500

    Various refactoring things. Added all directions for entities. Added check if an entity is underpopulated. TODO - this is ugly. Can be done in a better way by iterating through the struct fields most likely

--- entity/entity.go
+++ entity/entity.go
@@ -2,45 +2,115 @@ package entity
 
 //Entity represents a life-form in Conway's Game of Life
 type Entity struct {
-	up, down, left, right *Entity
+	north, south, west, east, northwest, northeast, southwest, southeast *Entity
 }
 
-//Up returns the reference to the neighbor above the calling entity
-func (entity *Entity) Up() *Entity {
-	return entity.up
+//IsUnderpopulated returns true if the total number of neighbors for the entity is less than 2
+func (entity Entity) IsUnderpopulated() bool {
+	var neighborCount = 0
+	if entity.northwest != nil {
+		neighborCount++
+	}
+	if entity.north != nil {
+		neighborCount++
+	}
+	if entity.northeast != nil {
+		neighborCount++
+	}
+	if entity.east != nil {
+		neighborCount++
+	}
+	if entity.southeast != nil {
+		neighborCount++
+	}
+	if entity.south != nil {
+		neighborCount++
+	}
+	if entity.southwest != nil {
+		neighborCount++
+	}
+	if entity.west != nil {
+		neighborCount++
+	}
+	return neighborCount < 2
 }
 
-//Down returns the reference to the neighbor below the calling entity
-func (entity *Entity) Down() *Entity {
-	return entity.down
+//North returns the reference to the neighbor north the calling entity
+func (entity *Entity) North() *Entity {
+	return entity.north
 }
 
-//Left returns the reference to the neighbor left of the calling entity
-func (entity *Entity) Left() *Entity {
-	return entity.left
+//South returns the reference to the neighbor south the calling entity
+func (entity *Entity) South() *Entity {
+	return entity.south
 }
 
-//Right returns the reference to the neighbor right of the calling entity
-func (entity *Entity) Right() *Entity {
-	return entity.right
+//West returns the reference to the neighbor west of the calling entity
+func (entity *Entity) West() *Entity {
+	return entity.west
 }
 
-//SetUp sets the reference to the neighbor above the calling entity
-func (entity *Entity) SetUp(upEntity *Entity) {
-	entity.up = upEntity
+//East returns the reference to the neighbor east of the calling entity
+func (entity *Entity) East() *Entity {
+	return entity.east
 }
 
-//SetDown sets the reference to the neighbor below the calling entity
-func (entity *Entity) SetDown(downEntity *Entity) {
-	entity.down = downEntity
+//Northwest returns the reference to the neighbor northwest of the calling entity
+func (entity *Entity) Northwest() *Entity {
+	return entity.northwest
 }
 
-//SetLeft sets the reference to the neighbor below the calling entity
-func (entity *Entity) SetLeft(leftEntity *Entity) {
-	entity.left = leftEntity
+//Northeast returns the reference to the neighbor northeast of the calling entity
+func (entity *Entity) Northeast() *Entity {
+	return entity.northeast
 }
 
-//SetRight sets the reference to the neighbor below the calling entity
-func (entity *Entity) SetRight(rightEntity *Entity) {
-	entity.right = rightEntity
+//Southhwest returns the reference to the neighbor southwest of the calling entity
+func (entity *Entity) Southwest() *Entity {
+	return entity.southwest
+}
+
+//Southeast returns the reference to the neighbor southeast of the calling entity
+func (entity *Entity) Southeast() *Entity {
+	return entity.southeast
+}
+
+//SetNorth sets the reference to the neighbor north of the calling entity
+func (entity *Entity) SetNorth(northEntity *Entity) {
+	entity.north = northEntity
+}
+
+//SetSouth sets the reference to the neighbor south of the calling entity
+func (entity *Entity) SetSouth(southEntity *Entity) {
+	entity.south = southEntity
+}
+
+//SetWest sets the reference to the neighbor west of the calling entity
+func (entity *Entity) SetWest(westEntity *Entity) {
+	entity.west = westEntity
+}
+
+//SetEast sets the reference to the neighbor east of the calling entity
+func (entity *Entity) SetEast(eastEntity *Entity) {
+	entity.east = eastEntity
+}
+
+//SetNorthwest sets the reference to the neighbor northwest of the calling entity
+func (entity *Entity) SetNorthwest(northwestEntity *Entity) {
+	entity.northwest = northwestEntity
+}
+
+//SetNortheast sets the reference to the neighbor northeast of the calling entity
+func (entity *Entity) SetNortheast(northeastEntity *Entity) {
+	entity.northeast = northeastEntity
+}
+
+//SetSouthwest sets the reference to the neighbor southwest of the calling entity
+func (entity *Entity) SetSouthwest(southwestEntity *Entity) {
+	entity.southwest = southwestEntity
+}
+
+//SetSoutheast sets the reference to the neighbor southeast of the calling entity
+func (entity *Entity) SetSoutheast(southeastEntity *Entity) {
+	entity.southeast = southeastEntity
 }
--- entity/entity_test.go
+++ entity/entity_test.go
@@ -1,64 +1,165 @@
 package entity
 
 import (
-	"testing"
+	. "testing"
 )
 
-func TestShouldGetUpEntity(t *testing.T) {
-	var someEntity Entity
-	var someOtherEntity Entity
-	someEntity.SetUp(&someOtherEntity)
-	actualEntity := someEntity.Up()
+type entityTestScenarios struct {
+	inputEntity Entity
+	result      bool
+}
 
-	if actualEntity == nil {
-		t.Error("Up entity not assigned")
+func TestShouldDetermineUnderpopulation(t *T) {
+	underpopulationScenarios := []entityTestScenarios{
+		{entity(nil, nil, nil, nil, nil, nil, nil, nil), true},
+		{entity(&Entity{}, nil, nil, nil, nil, nil, nil, nil), true},
+		{entity(nil, &Entity{}, nil, nil, nil, nil, nil, nil), true},
+		{entity(nil, nil, &Entity{}, nil, nil, nil, nil, nil), true},
+		{entity(nil, nil, nil, &Entity{}, nil, nil, nil, nil), true},
+		{entity(nil, nil, nil, nil, &Entity{}, nil, nil, nil), true},
+		{entity(nil, nil, nil, nil, nil, &Entity{}, nil, nil), true},
+		{entity(nil, nil, nil, nil, nil, nil, &Entity{}, nil), true},
+		{entity(nil, nil, nil, nil, nil, nil, nil, &Entity{}), true},
+		{entity(&Entity{}, &Entity{}, nil, nil, nil, nil, nil, nil), false},
+		{entity(nil, nil, &Entity{}, nil, nil, &Entity{}, nil, nil), false},
+		{entity(nil, nil, nil, &Entity{}, nil, nil, nil, &Entity{}), false},
 	}
-	if actualEntity != &someOtherEntity {
-		t.Error("Wrong up entity returned")
+	for _, scenario := range underpopulationScenarios {
+		isUnderpopulated := scenario.inputEntity.IsUnderpopulated()
+		if isUnderpopulated != scenario.result {
+			t.Fail()
+		}
 	}
 }
 
-func TestShouldGetDownEntity(t *testing.T) {
+func TestShouldGetNorthEntity(t *T) {
 	var someEntity Entity
 	var someOtherEntity Entity
-	someEntity.SetDown(&someOtherEntity)
-
-	actualEntity := someEntity.Down()
+	someEntity.SetNorth(&someOtherEntity)
+	actualEntity := someEntity.North()
 
 	if actualEntity == nil {
-		t.Error("Up entity not assigned")
+		t.Error("North entity not assigned")
 	}
 	if actualEntity != &someOtherEntity {
-		t.Error("Wrong up entity returned")
+		t.Error("Wrong entity returned")
 	}
 }
 
-func TestShouldGetLeftEntity(t *testing.T) {
+func TestShouldGetSouthEntity(t *T) {
 	var someEntity Entity
 	var someOtherEntity Entity
-	someEntity.SetLeft(&someOtherEntity)
+	someEntity.SetSouth(&someOtherEntity)
 
-	actualEntity := someEntity.Left()
+	actualEntity := someEntity.South()
 
 	if actualEntity == nil {
-		t.Error("Up entity not assigned")
+		t.Error("South entity not assigned")
 	}
 	if actualEntity != &someOtherEntity {
-		t.Error("Wrong up entity returned")
+		t.Error("Wrong entity returned")
 	}
 }
 
-func TestShouldGetRightEntity(t *testing.T) {
+func TestShouldGetWestEntity(t *T) {
 	var someEntity Entity
 	var someOtherEntity Entity
-	someEntity.SetRight(&someOtherEntity)
+	someEntity.SetWest(&someOtherEntity)
 
-	actualEntity := someEntity.Right()
+	actualEntity := someEntity.West()
 
 	if actualEntity == nil {
-		t.Error("Up entity not assigned")
+		t.Error("West entity not assigned")
 	}
 	if actualEntity != &someOtherEntity {
-		t.Error("Wrong up entity returned")
+		t.Error("Wrong entity returned")
 	}
+}
+
+func TestShouldGetEastEntity(t *T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.SetEast(&someOtherEntity)
+
+	actualEntity := someEntity.East()
+
+	if actualEntity == nil {
+		t.Error("East entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong entity returned")
+	}
+}
+
+func TestShouldGetNorthWestEntity(t *T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.SetNorthwest(&someOtherEntity)
+
+	actualEntity := someEntity.East()
+
+	if actualEntity == nil {
+		t.Error("Northwest entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong entity returned")
+	}
+}
+
+func TestShouldGetNorthEastEntity(t *T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.SetNortheast(&someOtherEntity)
+
+	actualEntity := someEntity.East()
+
+	if actualEntity == nil {
+		t.Error("Northeast entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong entity returned")
+	}
+}
+
+func TestShouldGetSouthWestEntity(t *T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.SetSouthwest(&someOtherEntity)
+
+	actualEntity := someEntity.East()
+
+	if actualEntity == nil {
+		t.Error("Southwest entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong entity returned")
+	}
+}
+
+func TestShouldGetSouthEastEntity(t *T) {
+	var someEntity Entity
+	var someOtherEntity Entity
+	someEntity.SetSoutheast(&someOtherEntity)
+
+	actualEntity := someEntity.East()
+
+	if actualEntity == nil {
+		t.Error("Southeast entity not assigned")
+	}
+	if actualEntity != &someOtherEntity {
+		t.Error("Wrong entity returned")
+	}
+}
+
+func entity(nw, n, ne, e, se, s, sw, w *Entity) Entity {
+	var testEntity Entity
+	testEntity.SetNorth(n)
+	testEntity.SetNorthwest(nw)
+	testEntity.SetNortheast(ne)
+	testEntity.SetEast(e)
+	testEntity.SetSoutheast(se)
+	testEntity.SetSouth(s)
+	testEntity.SetSouthwest(sw)
+	testEntity.SetWest(w)
+	return testEntity
 }
