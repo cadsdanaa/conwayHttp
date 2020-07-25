54cdd71d161f16f0f49a612e779c8be73bdd183a
Alex Dana <cadsdanaa@gmail.com>
2020-07-24 22:22:43 -0500

    Added high level universe creation stuff. Need to populate with some basic entities

--- universe/universe.go
+++ universe/universe.go
@@ -2,6 +2,14 @@ package universe
 
 import "github.com/cadsdanaa/conwayHttp/entity"
 
-func initialUniverse() [][]entity.Entity {
-	return [][]entity.Entity{}
+func InitialUniverse(universeSize int) [][]entity.Entity {
+	return initialGrid(universeSize)
+}
+
+func initialGrid(universeSize int) [][]entity.Entity {
+	var universe = make([][]entity.Entity, universeSize)
+	for i := 0; i < universeSize; i++ {
+		universe[i] = make([]entity.Entity, universeSize)
+	}
+	return universe
 }
--- universe/universe_test.go
+++ universe/universe_test.go
@@ -1,9 +1,28 @@
 package universe
 
-import "testing"
+import (
+	. "testing"
+)
 
-func TestShouldInitializeUniverse(t *testing.T) {
-	var universe = initialUniverse()
+func TestInitialUniverseShouldContainDeadEntities(t *T) {
+	t.Error("TODO - Need to implement")
+}
+
+func TestInitialUniverseShouldBeGivenSize(t *T) {
+	var size = 25
+	var universe = InitialUniverse(size)
+	if len(universe) != size {
+		t.Fail()
+	}
+	for _, row := range universe {
+		if len(row) != size {
+			t.Fail()
+		}
+	}
+}
+
+func TestShouldInitializeUniverse(t *T) {
+	var universe = InitialUniverse(20)
 
 	if universe == nil {
 		t.Fail()
